195944da1ebe147cb2cf17492f9d71617d8669bf
Mael Mbange <80531753+MaelMbange@users.noreply.github.com>
2024-09-17 01:10:01 +0200

    :recycle: Modifier Window Class

--- include/window.hpp
+++ include/window.hpp
@@ -2,25 +2,93 @@
 // Created by @IceCurim on 16/09/2024.
 //
 
-#ifndef WINDOW_HPP
-#define WINDOW_HPP
+#ifndef ENGINE_HPP
+#define ENGINE_HPP
 
+#include <iostream>
 #include <GLFW/glfw3.h>
 #include <utils/logger.hpp>
 
-
-namespace divide_engine {
-    class Window {
-        GLFWwindow* window = nullptr;
-        std::shared_ptr<Logger> logger;
+namespace divide_engine{
+    /**
+     * @class Window
+     * @brief Manages the creation and handling of a window.
+     */
+    class Window{
+    private:
+     std::shared_ptr<Logger> logger = Logger::get_instance();
+     /**
+     * @brief Window parameters
+     * @param width : window width
+     * @param height : window height
+     * @param title : window title
+     * @param window : window object that will be created by GLFW
+     */
+     int width = 0;
+     int height = 0;
+     std::string title;
+     GLFWwindow* window = nullptr;
 
     public:
-        Window();
-        ~Window();
+     Window(int width, int height, std::string title);
+     ~Window();
 
-        [[nodiscard]] bool is_created() const;
-        void close() const;
+     void open();
+
+     /**
+      * @brief Checks if the @window is created (open).
+      * @return true if the @window is created (open), false otherwise.
+      */
+     [[nodiscard]] bool is_created()const;
+
+     /**
+      * @brief Sets default window parameters.
+      */
+     void set_defaults() const;
+
+     /**
+      * @brief Sets whether the window is resizable.
+      * This is ignored for full screen windows and undecorated window (when @set_decorated is set to false).
+      * @param resizable : true if the window should be resizable, false otherwise. By default, is true.
+      */
+     void set_resizable(bool resizable) const;
+
+     /**
+      * @brief Sets the visibility of the window.
+      * This is ignored for full screen windows.
+      * @param visible : true if the window should be visible, false otherwise. By default, is true.
+      */
+     void set_visible(bool visible) const;
+
+     /**
+      * @brief Sets whether the window is decorated (has borders and controls).
+      * This is ignored for full screen windows.
+      * @param decorated : true if the window should be decorated, false otherwise. By default, is true.
+      */
+     void set_decorated(bool decorated) const;
+
+     /**
+      * @brief Sets whether the window is focused.
+      * This is ignored for full screen windows.
+      * @param focused : true if the window should be focused, false otherwise. By default, is true.
+      */
+     void set_focused(bool focused) const;
+
+     /**
+      * @brief Sets whether the window is floating. Will the window be minimized when it loses focus?
+      * This is ignored for windowed windows.
+      * @param auto_iconify : true if the window should be minimized, false otherwise. By default, is false.
+      */
+     void set_auto_iconify(bool auto_iconify) const;
+
+     /**
+     * @brief Sets whether the window is floating above other windows (top-most or always-on-top).
+     * This is ignored for full screen windows.
+     * @param floating : true if the window should be floating, false otherwise. By default, is false.
+     */
+     void set_floating(bool floating) const;
+
     };
 }
 
-#endif //WINDOW_HPP
+#endif //ENGINE_HPP
--- src/window.cpp
+++ src/window.cpp
@@ -1,21 +1,106 @@
 //
 // Created by @IceCurim on 16/09/2024.
 //
+
 #include "window.hpp"
 
+#include <cmath>
+#include <GLFW/glfw3.h>
+#include <utility>
 
 using namespace divide_engine;
+using namespace std::string_literals;
 
-Window::Window() : logger(Logger::get_instance(true,true)){
+Window::Window(const int width, const int height, std::string title) : width(width), height(height), title(std::move(title)) {
+        logger->log("Window created", LogLevel(LogLevel::Level::SUCCESS));
 }
 
 Window::~Window() {
+    if(is_created()) {
+        glfwDestroyWindow(window);
+        logger->log("Window destroyed", LogLevel(LogLevel::Level::TERMINATED));
+    }
 }
 
-bool Window::is_created() const{
-    return true;
+void Window::open() {
+    window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+    if(is_created()) {
+        logger->log("Window opened", LogLevel(LogLevel::Level::SUCCESS));
+
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        logger->log("Window closed", LogLevel(LogLevel::Level::CLOSED));
+    }else {
+        logger->log("Window failed to open", LogLevel(LogLevel::Level::ERROR));
+    }
 }
 
-void Window::close() const {
-    glfwDestroyWindow(window);
+bool Window::is_created() const {
+    return window != nullptr;
+}
+
+void Window::set_defaults() const {
+    if(!is_created()) {
+        glfwDefaultWindowHints();
+        logger->log("Set window defaults", LogLevel(LogLevel::Level::INFO));
+    }else{
+        logger->log("Set window defaults failed", LogLevel(LogLevel::Level::ERROR));
+    }
+}
+
+void Window::set_resizable(const bool resizable) const{
+    if(!is_created()) {
+        glfwWindowHint(GLFW_RESIZABLE, resizable? GLFW_TRUE : GLFW_FALSE);
+        logger->log("Set window hint: __resizable__", LogLevel(LogLevel::Level::INFO));
+    }else{
+        logger->log("Set window hint: __resizable__, must be set before creating the window (open)", LogLevel(LogLevel::Level::ERROR));
+    }
+}
+
+void Window::set_visible(const bool visible) const {
+    if(!is_created()) {
+        glfwWindowHint(GLFW_VISIBLE, visible? GLFW_TRUE : GLFW_FALSE);
+        logger->log("Set window hint: __visible__"s.append(" = ").append(visible?"TRUE":"FALSE"), LogLevel(LogLevel::Level::INFO));
+    }else{
+        logger->log("Set window hint: __visible__, must be set before creating the window (open)", LogLevel(LogLevel::Level::ERROR));
+    }
+}
+
+void Window::set_decorated(const bool decorated) const {
+    if(!is_created()) {
+        glfwWindowHint(GLFW_DECORATED, decorated? GLFW_TRUE : GLFW_FALSE);
+        logger->log("Set window hint: __decorated__"s.append(" = ").append(decorated?"TRUE":"FALSE"), LogLevel(LogLevel::Level::INFO));
+    }else{
+        logger->log("Set window hint: __decorated__, must be set before creating the window (open)", LogLevel(LogLevel::Level::ERROR));
+    }
+}
+
+void Window::set_focused(const bool focused) const {
+    if(!is_created()) {
+        glfwWindowHint(GLFW_FOCUSED, focused? GLFW_TRUE : GLFW_FALSE);
+        logger->log("Set window hint: __focused__"s.append(" = ").append(focused?"TRUE":"FALSE"), LogLevel(LogLevel::Level::INFO));
+    }else{
+        logger->log("Set window hint: __focused__, must be set before creating the window (open)", LogLevel(LogLevel::Level::ERROR));
+    }
+}
+
+void Window::set_auto_iconify(const bool auto_iconify) const {
+    if(!is_created()) {
+        glfwWindowHint(GLFW_AUTO_ICONIFY, auto_iconify? GLFW_TRUE : GLFW_FALSE);
+        logger->log("Set window hint: __auto_iconify__"s.append(" = ").append(auto_iconify?"TRUE":"FALSE"), LogLevel(LogLevel::Level::INFO));
+    }else{
+        logger->log("Set window hint: __auto_iconify__, must be set before creating the window (open)", LogLevel(LogLevel::Level::ERROR));
+    }
+}
+
+void Window::set_floating(const bool floating) const {
+    if(!is_created()) {
+        glfwWindowHint(GLFW_FLOATING, floating? GLFW_TRUE : GLFW_FALSE);
+        logger->log("Set window hint: __floating__"s.append(" = ").append(floating?"TRUE":"FALSE"), LogLevel(LogLevel::Level::INFO));
+    }else{
+        logger->log("Set window hint: __floating__, must be set before creating the window (open)", LogLevel(LogLevel::Level::ERROR));
+    }
 }
